b2b164ea424b5bc02c9bc685cf0dd985bfbf9a6e
turtleknight31 <70942416+turtleknight31@users.noreply.github.com>
2021-06-07 22:06:24 +0300

    Update MyFirstGame.java

--- lesson_1/MyFirstGame.java
+++ lesson_1/MyFirstGame.java
@@ -1,21 +1,19 @@
 import java.util.Scanner;
+
 public class MyFirstGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число: ");
-        int a = scanner.nextInt();
-        int number = 45;
-        while(number != a) {
-            if(number > a) {
+        int ComputerNumber = 45;
+        int UserNumber = 0;
+        do {
+            System.out.print("Введите число: ");
+            UserNumber = scanner.nextInt();
+            if(ComputerNumber > UserNumber) {
                 System.out.println("Данное число меньше того, что загадал компьютер");
-                System.out.println("Введите число: ");
-                a = scanner.nextInt();
-            } else if(number < a) {
+            } else if(ComputerNumber < UserNumber) {
                 System.out.println("Данное число больше того, что загадал компьютер");
-                System.out.println("Введите число: ");
-                a = scanner.nextInt();
             }
-        }
-        System.out.println("УРА УРА УРА! Поздравляю, число угадано!: " + a);
+        } while(ComputerNumber != UserNumber);
+        System.out.println("УРА УРА УРА! Поздравляю, число угадано!: " + UserNumber);
     }
 }
