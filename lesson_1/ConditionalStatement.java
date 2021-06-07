4d1c28a4a2de8c1fafca232aa2ad3e4421a4f093
turtleknight31 <70942416+turtleknight31@users.noreply.github.com>
2021-06-07 11:06:14 +0300

    Update ConditionalStatement.java

--- lesson_1/ConditionalStatement.java
+++ lesson_1/ConditionalStatement.java
@@ -1,14 +1,11 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
         int age = 23;
-        char firstLetterName = 'N';
-        boolean maleGender = true;
-        float height = 1.75f;
-
         if(age > 20) {
             System.out.println("Возраст пользователя: " + age);
         }
-
+        
+        boolean maleGender = true;
         if(maleGender) {
             System.out.println("Пол пользователя: Мужской" );
         }
@@ -17,12 +14,14 @@ public class ConditionalStatement {
             System.out.println("Пол пользователя: Женский" );
         }
 
+        float height = 1.75f;
         if(height < 1.80) {
             System.out.println("Рост пользователя ниже 180 см: " + height);
         } else {
             System.out.println("Рост пользователя выше 180 см: " + height);
         }
-
+        
+        char firstLetterName = 'N';
         if(firstLetterName == 'M') {
             System.out.println("Первая буква в имени пользователя начинается с " + firstLetterName);
         } else if(firstLetterName == 'I') {
