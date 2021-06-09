afb099ad67886f6fc6761ed0922cc82428304c85
turtleknight31 <70942416+turtleknight31@users.noreply.github.com>
2021-06-09 09:19:40 +0300

    Create Person.java

---
+++ lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String male = 'Man';
+    String name = "Nurzat";
+    float height = 1.75f;
+    float weight = 65.5f;
+    int age = 23;
+
+    void go() {
+        System.out.println("Вы идете");
+    }
+
+    String sit() {
+        retun "Вы сели";
+    }
+
+    boolean run() {
+        return true;
+    }
+
+    void talk() {
+        System.out.println("Вы говорите");
+    }
+
+    String studyJava() {
+        retrun "activated"
+    }
+}
