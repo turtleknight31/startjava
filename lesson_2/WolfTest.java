8f1ae599ac77cd6f9dfdc7780545f5e34032aadf
turtleknight31 <70942416+turtleknight31@users.noreply.github.com>
2021-06-09 22:37:21 +0300

    Update WolfTest.java

--- lesson_2/WolfTest.java
+++ lesson_2/WolfTest.java
@@ -1,16 +1,16 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolfChoice = new Wolf();
-        wolfChoice.male = "Woman";
-        System.out.println("Пол: " + wolfChoice.male);
-        wolfChoice.name = "Alfa";
-        System.out.println("Кличка волка: " + wolfChoice.name);
-        wolfChoice.color = "White";
-        System.out.println("Окрас волка: " + wolfChoice.color);
-        wolfChoice.weight = 20.6f;
-        System.out.println("Вес: " + wolfChoice.weight);
-        wolfChoice.age = 5;
-        System.out.println("Возраст: " + wolfChoice.age);
+        wolfChoice.setMale("Man");
+        System.out.println("Пол: " + wolfChoice.getMale());
+        wolfChoice.setName("Alfa");
+        System.out.println("Кличка волка: " + wolfChoice.getName());
+        wolfChoice.setColor("White");
+        System.out.println("Окрас волка: " + wolfChoice.getName());
+        wolfChoice.setWeight(15.6f);
+        System.out.println("Вес: " + wolfChoice.getWeight());
+        wolfChoice.setAge(9);
+        System.out.println("Возраст: " + wolfChoice.getAge());
         System.out.println("Метод walk: " + wolfChoice.walk());
         System.out.println("Метод sit: " + wolfChoice.sit());
         System.out.println("Метод run: " + wolfChoice.run());
