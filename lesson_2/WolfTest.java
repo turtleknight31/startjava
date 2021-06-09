a935ecdf6a30588a3eeb4223628eee6128781b69
turtleknight31 <70942416+turtleknight31@users.noreply.github.com>
2021-06-09 19:34:07 +0300

    Update WolfTest.java

--- lesson_2/WolfTest.java
+++ lesson_2/WolfTest.java
@@ -1,10 +1,15 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolfChoice = new Wolf();
+        wolfChoice.male = "Woman";
         System.out.println("Пол: " + wolfChoice.male);
+        wolfChoice.name = "Alfa";
         System.out.println("Кличка волка: " + wolfChoice.name);
+        wolfChoice.color = "White";
         System.out.println("Окрас волка: " + wolfChoice.color);
+        wolfChoice.weight = 20.6f;
         System.out.println("Вес: " + wolfChoice.weight);
+        wolfChoice.age = 5;
         System.out.println("Возраст: " + wolfChoice.age);
         System.out.println("Метод walk: " + wolfChoice.walk());
         System.out.println("Метод sit: " + wolfChoice.sit());
