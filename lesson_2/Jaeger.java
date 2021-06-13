5a637f1f854bf9832563c16a53f6a66d7bc516e2
turtleknight31 <70942416+turtleknight31@users.noreply.github.com>
2021-06-13 13:48:15 +0300

    Update Jaeger.java

--- lesson_2/Jaeger.java
+++ lesson_2/Jaeger.java
@@ -1,13 +1,23 @@
 public class Jaeger {
-    
-    private String modelName;
-    private String mark;
-    private String origin;
-    private float height;
-    private float weight;
-    private int speed;
-    private int strength;
-    private int armor;
+        String modelName;
+        String mark;
+        String origin;
+        float height;
+        float weight;
+        int speed;
+        int strength;
+        int armor;
+
+    public Jaeger(String modelName, String mark, String origin, float height, float weight, int speed, int strength, int armor) {
+        this.modelName = modelName;
+        this.mark =  mark;
+        this.origin = origin;
+        this.height = height;
+        this.weight = weight;
+        this.speed = speed;
+        this.strength = strength;
+        this.armor = armor;
+    }
 
     public String getModuleName() {
         return modelName;
