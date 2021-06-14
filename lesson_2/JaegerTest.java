c2f26062d50dd1956a1eca46d54a37ee6d8c6b48
turtleknight31 <70942416+turtleknight31@users.noreply.github.com>
2021-06-14 13:53:20 +0300

    Update JaegerTest.java

--- lesson_2/JaegerTest.java
+++ lesson_2/JaegerTest.java
@@ -1,6 +1,5 @@
 public class JaegerTest {
     public static void main(String[] args) {
-
         Jaeger jaegerOne = new Jaeger("Black Maria", "Mark-III", "Mexico", 320.4f, 418.54f, 45, 4, 6);
         Jaeger jaegerTwo = new Jaeger("Gipsy Danger", "Mark-III", "USA", 340.5f, 450.54f, 50, 5, 8);
 
