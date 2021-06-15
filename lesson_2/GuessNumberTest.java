14829e52bbe78dc85331707d96d7afcad0fb5987
turtleknight31 <70942416+turtleknight31@users.noreply.github.com>
2021-06-15 10:07:42 +0300

    Update GuessNumberTest.java

--- lesson_2/GuessNumberTest.java
+++ lesson_2/GuessNumberTest.java
@@ -4,18 +4,18 @@ public class GuessNumberTest {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String choice = "yes";
-        
+
         do {
             GuessNumber game = new GuessNumber();
             System.out.print("Введите имя первого игрока: ");
             game.firstPlayer();
             System.out.print("Введите имя второго игрока: ");
             game.secondPlayer();
+
         
             do {
                 game.start();
             } while(game.getLuckyShot() != 1);
-            
             do {
                 System.out.print("Вы хотите продолжить?[yes/no]: ");
                 choice = scan.next();
