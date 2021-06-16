f310a8307bac4f67cc5ed83073b4094e506d24d6
turtleknight31 <70942416+turtleknight31@users.noreply.github.com>
2021-06-16 15:47:48 +0300

    Add files via upload

---
+++ src/com/startjava/lesson_1/calculator/Calculator.java
@@ -0,0 +1,32 @@
+public class Calculator {
+    public static void main(String[] args) {
+        char sign = '^';
+        float num1 = 3.4f;
+        float num2 = 5f;
+        float result = 1.0F;
+
+        if(sign == '+') {
+            result = num1 + num2;
+            System.out.println("Сумма num1 и num2 будет равным: " + result);
+        } else if(sign == '-') {
+            result = num1 - num2;
+            System.out.println("Разность num1 и num2 будет равным: " + result);
+        } else if(sign == '*') {
+            result = num1 * num2;
+            System.out.println("Умножения num1 и num2 будет равным: " + result);
+        } else if(sign == '/') {
+            result = num1 / num2;
+            System.out.println("Деления num1 и num2 будет равным: " + result);
+        } else if(sign == '^') {
+            for(int i = 1; i <= num2; i++) {
+                result *= num1;
+            }
+            System.out.println("num1 в степени num2 будет равным: " + result);
+        } else if(sign == '%') {
+            result = num1 % num2;
+            System.out.println("Остаток от деления будет равным: " + result);
+        } else {
+            System.out.println("Вы ввели не корректный символ, введите пожалуйста заново: " + sign);
+        }
+    }
+}
