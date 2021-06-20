fcba69a57f0919c6a9b413f8343e3a2e0d18b26b
turtleknight31 <s.nurzat.1997@gmail.com>
2021-06-20 22:03:59 +0300

    new lesson

---
+++ src/com/startjava/lesson_2_3_4/arrayeven/ArrayEven.java
@@ -0,0 +1,30 @@
+package lesson_2_3_4.arrayeven;
+
+import java.util.Scanner;
+
+public class ArrayEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число N ");
+        int n = scanner.nextInt();
+        int[] mass = new int[n];
+        System.out.println("Введите числа с пробелом: ");
+
+        for(int i = 0; i < n; i++) {
+            mass[i] = scanner.nextInt();
+        }
+
+        int[] even = new int[n];
+        int j = 0;
+        for(int i = 0; i < n; i++) {
+            if(mass[i] % 2 == 0) {
+                even[j] = mass[i];
+                j++;
+            }
+        }
+
+        for(int i = 0; i < j; i++) {
+            System.out.println("Число: " + even[i]);
+        }
+    }
+}
