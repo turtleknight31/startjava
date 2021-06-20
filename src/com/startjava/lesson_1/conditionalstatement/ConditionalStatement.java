4c44e02d9cf0b385ead2c8be5f911a9cdde03446
turtleknight31 <s.nurzat.1997@gmail.com>
2021-06-20 17:04:36 +0300

    g

---
+++ src/com/startjava/lesson_1/conditionalstatement/ConditionalStatement.java
@@ -0,0 +1,41 @@
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        int age = 23;
+        char firstLetterName = 'N';
+        boolean maleGender = true;
+        float height = 1.75f;
+
+        if(age > 20) {
+            System.out.println("Возраст пользователя: " + age);
+        }
+
+        if(maleGender) {
+            System.out.println("Пол пользователя: Мужской" );
+        }
+
+        if(!maleGender) {
+            System.out.println("Пол пользователя: Женский" );
+        }
+
+        if(height < 1.80) {
+            System.out.println("Рост пользователя ниже 180 см: " + height);
+        } else {
+            System.out.println("Рост пользователя выше 180 см: " + height);
+        }
+
+        if(firstLetterName == 'M') {
+            System.out.println("Первая буква в имени пользователя начинается с " + firstLetterName);
+        } else if(firstLetterName == 'I') {
+            System.out.println("Первая буква в имени пользователя начинается с " + firstLetterName);
+        } else {
+            System.out.println("Первая буква в имени пользователя начинается с " + firstLetterName);
+        }
+    }
+}
+
+
+
+
+
+
+
