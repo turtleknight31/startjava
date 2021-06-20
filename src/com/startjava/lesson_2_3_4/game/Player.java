acbdf0f23f27177291cb94c1b964c7561eb6c7ce
turtleknight31 <s.nurzat.1997@gmail.com>
2021-06-20 16:57:38 +0300

    g

---
+++ src/com/startjava/lesson_2_3_4/game/Player.java
@@ -0,0 +1,14 @@
+package src.com.startjava.lesson_2_3_4.game;
+
+public class Player {
+    private String name;
+
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/com/startjava/lesson_2_3_4/person/Person.java
@@ -0,0 +1,30 @@
+package src.com.startjava.lesson_2_3_4.person;
+
+public class Person {
+
+    String male = "Man";
+    String name = "Nurzat";
+    float height = 1.75f;
+    float weight = 65.5f;
+    int age = 23;
+
+    void go() {
+        System.out.println("Вы идете");
+    }
+
+    String sit() {
+        return "Вы сели";
+    }
+
+    boolean run() {
+        return true;
+    }
+
+    void talk() {
+        System.out.println("Вы говорите");
+    }
+
+    String studyJava() {
+        return "activated";
+    }
+}
