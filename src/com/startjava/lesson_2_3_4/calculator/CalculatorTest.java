0b6ada437d62823a4a251c0e8bb23bac8fb14c3e
turtleknight31 <s.nurzat.1997@gmail.com>
2021-06-28 16:14:51 +0300

    26

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -3,7 +3,7 @@ package com.startjava.lesson_2_3_4.calculator;
 import java.util.Scanner;
 
 public class CalculatorTest {
-    private static String choice = "yes";
+    static boolean isNext;
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -12,16 +12,20 @@ public class CalculatorTest {
             Calculator calculator = new Calculator(scan.nextLine());
             System.out.println("Результат: " + calculator.calculate());
             choice();
-        } while(choice.equals("yes"));
+        } while(!isNext);
         System.out.println("Вы завершили вычисления!");
     }
 
     public static void choice() {
+        String choice;
         do {
             System.out.print("Вы хотите продолжить? [yes/no]: ");
             choice = scan.nextLine();
             if((!choice.equals("no")) && (!choice.equals("yes"))) {
                 System.out.println("Повторите пожалуйста ввод: ");
+            }
+            if(choice.equals("no")) {
+                isNext = true;
             }
         } while(!choice.equals("no") && !choice.equals("yes"));
     }
