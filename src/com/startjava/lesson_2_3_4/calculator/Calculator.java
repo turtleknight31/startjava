066d1515908d01333abb9ae21e725df04d9e4593
turtleknight31 <s.nurzat.1997@gmail.com>
2021-06-28 11:26:53 +0300

    28-06

---
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -0,0 +1,31 @@
+package com.startjava.lesson_2_3_4.calculator;
+
+public class Calculator {
+    private String mathExpression;
+
+    public Calculator(String mathExpression) {
+        this.mathExpression = mathExpression;
+    }
+
+    public int calculate() {
+        String[] words = mathExpression.split(" ");
+        int num1 = Integer.parseInt(words[0]);
+        int num2 = Integer.parseInt(words[2]);
+
+        switch(words[1]) {
+            case "+":
+                return Math.addExact(num1, num2);
+            case "-":
+                return Math.subtractExact(num1, num2);
+            case "*":
+                return Math.multiplyExact(num1, num2);
+            case "/":
+                return num1 / num2;
+            case "^":
+                return (int) Math.pow(num1, num2);
+            case "%":
+                return num1 % num2;
+        }
+        return 0;
+    }
+}
---
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -0,0 +1,29 @@
+package com.startjava.lesson_2_3_4.calculator;
+
+import java.util.Scanner;
+
+public class CalculatorTest {
+    private static String choice = "yes";
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        do {
+            System.out.print("Введите выражение через пробел: ");
+            Calculator calculator = new Calculator(scan.nextLine());
+            System.out.println("Результат: " + calculator.calculate());
+            choice();
+        } while(choice.equals("yes"));
+        System.out.println("Вы завершили вычисления!");
+    }
+
+    public static void choice() {
+        do {
+            choice = "yes";
+            System.out.print("Вы хотите продолжить?[yes/no]: ");
+            choice = scan.nextLine();
+            if((!choice.equals("no")) && (!choice.equals("yes"))) {
+                System.out.println("Повторите пожалуйста ввод: ");
+            }
+        } while(!choice.equals("no") && !choice.equals("yes"));
+    }
+}
